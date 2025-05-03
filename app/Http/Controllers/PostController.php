a11f41000d0658fc2bf7ca29fdc03bcf9d9e2799
ZeragonAnachronised <ioanidiilya@gmail.com>
2025-05-03 19:28:46 +0600

    all files functions done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,63 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function upload(Request $request) {
+        $rules = [
+            'file' => 'required',
+            'name' => 'required|string|max:16|min:4'
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+        else {
+            $file = $request->file;
+            $name = $file->hashName();
+            $extension = $file->extension();
+            $post = new Post();
+            $post->filename = $name;
+            $post->save();
+            Storage::disk('public')->putFileAs('/', $request->file, $post->filename);
+            return response()->json([
+                'success' => true,
+                'filename' => $post->filename
+            ], 200);
+        }
+    }
+    public function download($file_id) {
+        $file_name = Post::where('id', $file_id)->first();
+        if($file_name->count()) {
+            return Storage::disk('public')->download($file_name->filename);
+        }
+        else {
+            return response()->json([
+                'success' => false,
+                'message' => 'not found'
+            ], 404);
+        }
+    }
+    public function show($file_id) {
+        $file_name = Post::where('id', $file_id)->first();
+        if($file_name->count()) {
+            $file_name = $file_name->filename;
+            return response()->file(Storage::disk('public')->path($file_name));
+        }
+        else {
+            return response()->json([
+                'success' => false,
+                'message' => 'not found'
+            ], 404);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/logout', [UserController::class, 'logout']);
     Route::get('/allout', [UserController::class, 'allout']);
     Route::get('/destroy', [UserController::class, 'destroy']);
+    Route::post('/upload', [PostController::class, 'upload']);
+    Route::get('/download/{file_id}', [PostController::class, 'download']);
+    Route::get('/show/{file_id}', [PostController::class, 'show']);
 });
 Route::post('/registration', [UserController::class, 'registration']);
 Route::post('/authentication', [UserController::class, 'authentication']);
